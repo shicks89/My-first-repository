376a67f35568ac9c51716079a371cbaf014ebff4
shicks89 <shicks89@ivytech.edu>
2024-11-27 12:09:19 -0500

    Add files via upload
    From Cats and Dogs Python Assignment

---
+++ Cats and Dogs Python Assignment.py
@@ -0,0 +1,10 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Variable to store the word "cats"
+>>> c = "cats"
+>>> # Variable to store the word "dogs."
+>>> d = "dogs."
+>>> # Constructing the sentence to print
+>>> s = f"It is raining {c} and {d}"
+>>> print (s)
+It is raining cats and dogs.
